84332e51207bef380bc50914d0175f04fc343122
Randygarcia24 <garciahernandezrandy@gmail.com>
2023-11-18 16:37:00 -0500

    agregando backend

---
+++ src/routes/backend/login.java
@@ -0,0 +1,45 @@
+
+public class login 
+{  
+    private int id;
+    private String nombre;
+    private String pass;
+
+    public login() {
+    }
+    
+
+    public login(int id, String nombre, String pass) 
+    {
+        this.id = id;
+        this.nombre = nombre;
+        this.pass = pass;
+    }
+
+    public int getId() 
+    {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+    
+    
+}
---
+++ src/routes/backend/loginDAO.java
@@ -0,0 +1,38 @@
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class LoginDAO 
+{
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
+    conexion cn = new conexion();
+    
+    
+    public login log(String nombre, String pass)
+    {
+        login l = new login();
+        String sql = "SELECT * FROM usuarios WHERE nombre = ? AND pass = ?";
+        try
+        {
+            con = cn.getConnection();
+            ps = con.prepareStatement(sql);
+            ps.setString(1, nombre);
+            ps.setString(2, pass);
+            rs = ps.executeQuery();
+            if(rs.next())
+            {
+                l.setId(rs.getInt("id"));
+                l.setNombre(rs.getString("nombre"));
+                l.setPass(rs.getString("pass"));
+                
+            }
+
+        }catch(SQLException e){System.out.println(e.toString());}
+        return l;
+    }
+}
